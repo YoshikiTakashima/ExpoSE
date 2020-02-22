68f590d943f21ec1e6998a4b10e49dd5f59f86de
Yoshiki Takashima <ytakashi@andrew.cmu.edu>
2020-02-21 23:47:55 -0500

    Updated url.js with bounds and added easier example

--- tests/web/easy_url.js
+++ tests/web/easy_url.js
@@ -2,7 +2,7 @@ var S$ = require('S$');
 var x = S$.symbol("X", '');
 S$.assume(x.length < 25);
 
-var url = 'https://experiment.com?' + x;
+var url = 'https://experiment.com?z=' + x + "&y=10&c=%3F";
 
 const hashes = url.slice(url.indexOf('?') + 1).split('&');
 const params = {}
